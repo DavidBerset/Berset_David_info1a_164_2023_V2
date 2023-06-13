1f03a8b23589077f367c376c1ca74cfb1a71584a
David Berset <david.berset.vx@gmail.com>
2023-06-13 08:18:35 +0200

    Encore la suite

--- Start.bat
+++ Start.bat
@@ -2,6 +2,6 @@
 echo "Démarrage en cours ..."
 echo "Installations des dépendances ..."
 pip install -r requirements.txt
-python.exe run_app.py
+python.exe run_mon_app.py
 echo "Fin du programme"
 pause
